5b0dee5050198337099f31d646c5b0e592f2d29e
hardik09978178 <68243533+hardik09978178@users.noreply.github.com>
2020-07-22 11:59:30 +0530

    Add files via upload

---
+++ ChngBG.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<body>
+
+		<h1>JavaScript CompilerResponse</h1>
+
+		<script>
+			function change(i) {
+		 var doc =      document.getElementById("background"); 
+var color =[ "black", "blue", "brown", "green"];
+	 for(i=0; i<color.length; i++){ 
+	doc.style.backgroundColor = color[i]; 
+	alert("my color is "+ color[i]); 
+	/*if (i>=color.length){i=0;}*/ 
+		} } 
+	setInterval(function changebackground()
+				{change(0)},1000);
+				
+				</script>
+	</body>
+</html>
